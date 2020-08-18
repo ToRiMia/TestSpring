b927f209037ad474cfbae9c7f70c84d5e58dd165
Viktoriia Omelchenko <omelchenko.viktoriia7@gmail.com>
2020-08-18 22:01:16 +0300

    Added init, destroy, factory method for musicBean

--- src/main/java/org/example/RockMusic.java
+++ src/main/java/org/example/RockMusic.java
@@ -1,6 +1,21 @@
 package org.example;
 
 public class RockMusic implements Music{
+    private RockMusic() {
+    }
+
+    public static RockMusic getRockMusic(){
+        return new RockMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialisation");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Demons";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,13 +7,16 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+
+        RockMusic rockMusic = context.getBean("musicBean", RockMusic.class);
+        System.out.println(rockMusic.getSong());
+        /*MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
         MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 
         //System.out.println("Name of player: " + musicPlayer.getName());
         musicPlayer2.setVolume(35);
         System.out.println("Volume 1: " + musicPlayer1.getVolume());
-        System.out.println("Volume 2: " + musicPlayer2.getVolume());
+        System.out.println("Volume 2: " + musicPlayer2.getVolume());*/
 
         context.close();
 
